b5edededceead27ebe7a66209191da2630dde550
Richard Udoh <udohrichard201114@gmail.com>
2022-01-14 15:06:16 +0000

    mastered how to play arround with strings

---
+++ playground/types.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+
+const name = 'richi';
+const middle = 'SUNDAY TOM';
+const last = `Udoh`;
+
+const sentence = 'She \'s so cool';
+const sentence2 = `She 's so cool`;
+
+
+ const song = `ohh I 
+ 
+ love you
+ 
+ 
+ Jesus`;
+
+
+// const hello = 'Hello my name is ' + name +  '. Nice to meet you';
+
+// let hello2 = 'Hello my name is ';
+
+// hello2 = hello2 + name + '.';
+// hello2 = hello2 + ' Nice to meet you';
+
+const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old`;
+
+const html = `
+
+    <div>
+        <h2>${name}</h2>
+        <p>${hello}</p>
+    </div>
+    
+    `;
+
+    document.body.innerHTML = html; 
